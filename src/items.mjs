a0ed11099c10161c0a3241b6947d718fd7dc139e
Juliusjev <122266761+Juliusjev@users.noreply.github.com>
2024-01-24 11:37:08 +0200

    Update items.mjs

--- src/items.mjs
+++ src/items.mjs
@@ -1,35 +1,29 @@
-import { json } from "body-parser";
-
 // mock data for simple API
 const items = [
-  {id: 1, name: 'Koira'},
-  {id: 2, name: 'Kissa'},
-  {id: 3, name: 'Hevonen'},
-  {id: 4, name: 'Virtahepo'},
+  {id: 1, name: 'Item 1'},
+  {id: 2, name: 'Item 2'},
+  {id: 3, name: 'Item kolme'},
+  {id: 4, name: 'Item neljä'},
 ];
 
 const getItems = (req, res) => {
   res.json(items);
 };
 
-
+// palauta vain se objekti, jonka id vastaa pyydettyä, muuten 404
 const getItemById = (req, res) => {
-  // TODO: palauta vain se objekti, jonka on id vastaa pyydettyä
   // console.log('requested item id', req.params.id);
-  const itemFound = items.find(item => {
-    return item.id == req.params.id;
-  });
+  const itemFound = items.find(item => item.id == req.params.id);
   // console.log('found item', itemFound);
-  // let item = items.find((item) => item.id === parseInt(req.params.id));
   if (itemFound) {
     res.json(itemFound);
   } else {
-    res.status(404);
-    res.json({error: 'not found'});
+    res.status(404).json({error: 'not found'});
   }
 };
 
 const postItem = (req, res) => {
+  // TODO: lisää postattu item items-taulukkoon
   res.json({message: 'item created'});
 };
 
@@ -46,4 +40,3 @@ const putItem = (req, res) => {
 
 
 export {getItems, getItemById, postItem, deleteItem, putItem};
-
